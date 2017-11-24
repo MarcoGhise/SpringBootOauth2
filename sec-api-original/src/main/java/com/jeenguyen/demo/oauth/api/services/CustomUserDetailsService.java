66592399ee07c7e0aadad91543a624a539f2c5e9
MarcoGhise <MarcoGhise@users.noreply.github.com>
2017-11-24 15:06:05 +0100

    Create CustomUserDetailsService.java

---
+++ sec-api-original/src/main/java/com/jeenguyen/demo/oauth/api/services/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.jeenguyen.demo.oauth.api.services;
+
+import com.jeenguyen.demo.oauth.api.entities.MongoUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created by jeebb on 11/8/14.
+ */
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+    	return new User(username, username, AuthorityUtils.createAuthorityList("ROLE_USER"));
+    	
+    }
+}
